fb46eea4473f76344db24c5188e8bf8c79ace10d
visualkhh <visulkhh@gmail.com>
2016-12-22 14:21:01 +0900

    modify

--- boot/default_template/src/main/java/com/khh/project/service/admin/domain/User.java
+++ boot/default_template/src/main/java/com/khh/project/service/admin/domain/User.java
@@ -36,7 +36,7 @@ public class User implements org.springframework.security.core.userdetails.UserD
 	//기본적인 하이버네이트로 EAGER 조인을 걸게되면 검색된 자식테이블만큼 부모것도 돌려줘버린다 이래서 아래 SELECT를 쓰면 각각 다시 조인쿼리날린다
 	//@Fetch(FetchMode.SELECT)  //http://stackoverflow.com/questions/19431021/hibernate-and-criteria-that-return-the-same-items-multiple-times
 	//또는 crit.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);  처럼 옵션을주면된다
-	@OneToMany
+	@OneToMany(fetch = FetchType.EAGER)
 	@JoinColumn(name="USERNAME")
 	private Collection<Authority> authorities;
 
