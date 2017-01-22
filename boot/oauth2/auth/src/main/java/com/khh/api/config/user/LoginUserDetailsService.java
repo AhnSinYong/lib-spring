fcfebb6322ea3a2294ac204f26e9107f7635ec44
khh <khh@omnicns.com>
2017-01-22 15:01:15 +0900

    db,jwt key

--- boot/oauth2/auth/src/main/java/com/khh/api/config/user/LoginUserDetailsService.java
+++ boot/oauth2/auth/src/main/java/com/khh/api/config/user/LoginUserDetailsService.java
@@ -1,13 +1,17 @@
 package com.khh.api.config.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 //@Transactional //http://jdm.kr/blog/141
 @Service
+@Slf4j
 public class LoginUserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService{
 	@Autowired
 	MessageSourceAccessor messageSource;
@@ -17,6 +21,8 @@ public class LoginUserDetailsService implements org.springframework.security.cor
 
 	@Override
 	public LoginUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		List<LoginUserDetails> list = userRepository.findAll();
+		//log.debug(list.size()+"");
 		LoginUserDetails userDetails = userRepository.findByUsername(username);
 		if (null == userDetails) {
 			throw new UsernameNotFoundException(messageSource.getMessage("error.login.fail"));
